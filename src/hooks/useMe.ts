70c0c36633469a8b3d551fc6ef92451f86e9cb23
Evmenov DK <dimitsky@icloud.com>
2024-08-10 19:46:56 +0400

    Добавил загрузку данных для авторизованного пользовател

---
+++ src/hooks/useMe.ts
@@ -0,0 +1,76 @@
+// react 
+import { useEffect, useState } from 'react';
+
+// type, context, consts
+import { IUser } from '../app/types';
+import { useAuth } from '../context/authContext/useAuth';
+import { BASE_URL } from '../app/consts';
+
+const URL = '/auth/me';
+
+type UseMe = {
+    data: IUser | null;
+    error: string | null;
+    isPending: boolean;
+}
+
+export function useMe(): UseMe {
+    const [ data, setData ] = useState<IUser | null>(null);
+    const [ error, setError ] = useState<string | null>(null);
+    const [ isPending, setIsPending ] = useState(false);
+    const { accessToken } = useAuth();
+
+    useEffect(() => {
+        const fetchMe = async () => {
+            const options = {
+                method: 'GET', 
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`, 
+                }, 
+            }
+
+            setIsPending(true);
+
+            try {
+                const response = await fetch(BASE_URL + URL, options);
+
+                if (response.ok) {
+                    const fetchedData = await response.json();
+
+                    setData(fetchedData);
+                    setError(null);
+                } else {
+                    if (response.status === 404) {
+                        throw new Error('404, not found');
+                    }
+
+                    if (response.status === 500) {
+                        throw new Error ('500, internal server error');
+                    }
+
+                    throw new Error(`${response.status}, ${response.statusText}`);
+                }
+            } catch (err) {
+                console.error(err);
+
+                if (err instanceof Error) {
+                    setError(err.message);
+                } else if (typeof err === 'string') {
+                    setError(err);
+                } else {
+                    setError('Unknown error');
+                }
+            } finally {
+                setIsPending(false);
+            }
+        }
+
+        fetchMe();
+    }, []);
+
+    return {
+        data, 
+        error, 
+        isPending, 
+    }
+}
--- src/pages/mePage/MePage.tsx
+++ src/pages/mePage/MePage.tsx
@@ -1,42 +1,70 @@
+import { Loader } from "../../comps/loader/Loader";
+import { useMe } from "../../hooks/useMe"
+
 export function MePage() {
+    const { data, error, isPending } = useMe();
+
+    if (isPending) {
+        return (
+            <Loader />
+        )
+    }
+
+    if (error) {
+        return (
+            <div>
+                <h1>
+                    Error
+                </h1>
+                <p>
+                    {error}
+                </p>
+            </div>
+        )
+    }
+    
     return (
         <div>
             <h1>
                 My page
             </h1>
-            <div>
-                <div>
-                    <img 
-                        src={'/'}
-                        alt={''}
-                    />
-                </div>
-                <div>
-                    <h2>
-                        Username: <span>{}</span>
-                    </h2>
-                </div>
-                <div>
-                    <h2>
-                        E-mail: <span>{}</span>
-                    </h2>
-                </div>
-                <div>
-                    <h2>
-                        First name: <span>{}</span>
-                    </h2>
-                </div>
-                <div>
-                    <h2>
-                        Last name: <span>{}</span>
-                    </h2>
-                </div>
-                <div>
-                    <h2>
-                        Gender: <span>{}</span>
-                    </h2>
-                </div>
-            </div>
+            {data ? (
+                    <div>
+                        <div>
+                            <img 
+                                src={data.image}
+                                alt={''}
+                            />
+                        </div>
+                        <div>
+                            <h2>
+                                Username: <span>{data.username}</span>
+                            </h2>
+                        </div>
+                        <div>
+                            <h2>
+                                E-mail: <span>{data.email}</span>
+                            </h2>
+                        </div>
+                        <div>
+                            <h2>
+                                First name: <span>{data.firstName}</span>
+                            </h2>
+                        </div>
+                        <div>
+                            <h2>
+                                Last name: <span>{data.lastName}</span>
+                            </h2>
+                        </div>
+                        <div>
+                            <h2>
+                                Gender: <span>{data.gender}</span>
+                            </h2>
+                        </div>
+                    </div>
+            ) : (
+                <></>
+            )}
         </div>
     )
 }
