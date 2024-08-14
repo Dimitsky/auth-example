d747cc7cee85dbc323cdefbab6158f26a5a10c84
Evmenov DK <dimitsky@icloud.com>
2024-08-14 12:20:09 +0400

    добавил стили

--- src/pages/mePage/MePage.tsx
+++ src/pages/mePage/MePage.tsx
@@ -1,5 +1,11 @@
+// comps
 import { Loader } from "../../comps/loader/Loader";
+
+// hooks
 import { useMe } from "../../hooks/useMe"
+
+// css
+import styles from './mePage.module.css';
 
 export function MePage() {
     const { data, error, isPending } = useMe();
@@ -24,42 +30,43 @@ export function MePage() {
     }
     
     return (
-        <div>
+        <div className={styles.wrap}>
             {data ? (
                 <>   
-                    <h1>
+                    <h1 className={styles.title}>
                         My page
                     </h1>
-                    <div>
-                        <div>
+                    <div className={styles.inner}>
+                        <div className={styles.imgWrap}>
                             <img 
+                                className={styles.img}
                                 src={data.image}
                                 alt={''}
                             />
                         </div>
-                        <div>
-                            <h2>
-                                Username: <span>{data.username}</span>
+                        <div className={styles.group}>
+                            <h2 className={styles.subTitle}>
+                                Username: <span className={styles.value}>{data.username}</span>
                             </h2>
                         </div>
-                        <div>
-                            <h2>
-                                E-mail: <span>{data.email}</span>
+                        <div className={styles.group}>
+                            <h2 className={styles.subTitle}>
+                                E-mail: <span className={styles.value}>{data.email}</span>
                             </h2>
                         </div>
-                        <div>
-                            <h2>
-                                First name: <span>{data.firstName}</span>
+                        <div className={styles.group}>
+                            <h2 className={styles.subTitle}>
+                                First name: <span className={styles.value}>{data.firstName}</span>
                             </h2>
                         </div>
-                        <div>
-                            <h2>
-                                Last name: <span>{data.lastName}</span>
+                        <div className={styles.group}>
+                            <h2 className={styles.subTitle}>
+                                Last name: <span className={styles.value}>{data.lastName}</span>
                             </h2>
                         </div>
-                        <div>
-                            <h2>
-                                Gender: <span>{data.gender}</span>
+                        <div className={styles.group}>
+                            <h2 className={styles.subTitle}>
+                                Gender: <span className={styles.value}>{data.gender}</span>
                             </h2>
                         </div>
                     </div>
